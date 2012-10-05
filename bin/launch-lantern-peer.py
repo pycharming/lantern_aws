66a5117a53563acfa4a8362fd698017ea4ba6701
Spider Person <aranhoide@gmail.com>
2012-10-05 03:48:43 +0200

    Python-based replacement for launch-lantern-peer.
    When calling launch-lantern-peer in ubuntu, the script passed in
    the user data was not being recognized by ubuntu CloudInit in the
    newly launched machine.  After unsuccessful attempts to debug
    that, I had more luck rewriting the script in Python.
    
    There were quite a few unix tools used in launch-lantern-peer and
    build-bootstrap, some of them with subtle incompatibilities.  I
    suspect some of these was the cause for this problem, but I
    haven't found it.  Some progress is made if you don't gzip the
    bootstrap script, but that still doesn't make it work.
    
    Python has equivalents for all those utilities in its standard
    library.  I suppose they are more compatible across platforms, but
    I haven't tested this in OS X yet.
    
    As an added benefit, you don't need to install the CloudFormation
    tools.  You do need to have a properly set up AWS_CREDENTIAL_FILE
    and `sudo pip install boto`.  I'll update the update-* scripts so
    you don't need to install the EC2 tools either.

---
+++ bin/launch-lantern-peer.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+# Watch for DRY (Don't Repeat Yourself) violation warnings.  If you change
+# these values, make sure to update unpack_template.py accordingly.
+
+import base64
+import os
+import random
+from cStringIO import StringIO
+import sys
+import tarfile
+import zlib
+
+from boto.cloudformation.connection import CloudFormationConnection
+
+
+assert __name__ == '__main__', "Don't import me, call me as a script."
+
+if len(sys.argv) != 2:
+    print "Launch a new lanter-peer node."
+    print "Usage: %s <node name>" % sys.argv[0]
+    sys.exit(1)
+
+here = os.path.dirname(sys.argv[0])
+stack_name = sys.argv[1]
+
+sio = StringIO()
+
+# DRY warning: mode.
+with tarfile.open(fileobj=sio, mode='w:bz2', dereference=True) as tf:
+    #DRY warning: bootstrap directory name.
+    tf.add(os.path.join(here, '..', 'bootstrap'),
+           arcname='bootstrap')
+blob = sio.getvalue()
+sanitized_blob = base64.b64encode(blob)
+
+template = file(os.path.join(here, 'unpack_template.py')).read()
+
+# DRY warning: placeholder string.
+self_extractable = template.replace("<PUT_BLOB_HERE>", sanitized_blob)
+
+#gzipped_b64 = base64.b64encode(zlib.compress(self_extractable, 9))
+gzipped_b64 = base64.b64encode(self_extractable)
+
+# The blob is broken into parts because each cloudformation template parameter
+# has a size limit.  This blob is reassembled inside the template to become
+# the user-data.
+sizelimit = 4096
+parts = [gzipped_b64[i*sizelimit:(i+1)*sizelimit]
+         for i in xrange(4)]
+
+ssl_proxy_port = random.randint(1024, 61024)
+
+parameters = ([("LanternSSLProxyPort", ssl_proxy_port)]
+              + zip(["Bootstrap", "Bootstrap2", "Bootstrap3", "Bootstrap4"],
+                    # Omit empty parts.
+                    filter(None, parts)))
+
+template = file(os.path.join(here,
+                             '..',
+                             'cloudformation',
+                             'lantern-peer.json')).read()
+
+
+conn = CloudFormationConnection()
+print conn.create_stack(stack_name,
+                        template_body=template,
+                        parameters=parameters)
