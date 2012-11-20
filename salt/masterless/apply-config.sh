dcbcd1036e4cd65c946515dbed0762d813af7344
Spider Person <aranhoide@gmail.com>
2012-11-20 01:50:23 +0100

    More verbose logging on applying salt updates.
    This is to debug a problem creating user lantern.

--- salt/masterless/apply-config.sh
+++ salt/masterless/apply-config.sh
@@ -6,5 +6,5 @@ DIRTY_FILE=${SALT_DIR}/.dirty
 if [[ -f ${DIRTY_FILE} || $1 == "--force" ]]; then
     rm -f ${DIRTY_FILE}
     logger "applying salt configuration updates..."
-    /usr/bin/salt-call state.highstate 2>&1 | logger
+    /usr/bin/salt-call -l debug state.highstate 2>&1 | tee /root/salt.log | logger
 fi
