a332b9d8628665b99c1da14f58f9a255ce91acff
Spider Person <aranhoide@gmail.com>
2012-12-22 00:00:39 +0100

    Add /usr/local paths to apply-config.sh .
    These are required by commands like pip and compass, which get
    their executables installed to /usr/local/bin.

--- salt/masterless/apply-config.sh
+++ salt/masterless/apply-config.sh
@@ -6,7 +6,7 @@ DIRTY_FILE=${SALT_DIR}/.dirty
 if [[ -f ${DIRTY_FILE} || $1 == "--force" ]]; then
     rm -f ${DIRTY_FILE}
     logger "applying salt configuration updates..."
-    PATH=/bin:/sbin:/usr/bin:/usr/sbin salt-call state.highstate 2>&1 | tee /root/salt.log | logger
+    PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin salt-call state.highstate 2>&1 | tee /root/salt.log | logger
     logger "done applying salt configuration updates."
     sudo -u lantern touch /home/lantern/.update-done
 fi
