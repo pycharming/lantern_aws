3afea1a9429b80656faf94596ce4c9feef98aaf7
Spider Person <aranhoide@gmail.com>
2013-06-13 17:51:19 +0200

    Setting config to release parameters.

--- bin/config.py
+++ bin/config.py
@@ -5,6 +5,6 @@ cloudmaster_name = 'cloudmaster'
 free_for_all_sg_name = 'free-for-all'
 
 # Override with values for testing.
-controller = 'fakectrl'
-cloudmaster_name = 'fakecloudmaster'
-region = 'us-east-1'
+#controller = 'fakectrl'
+#cloudmaster_name = 'fakecloudmaster'
+#region = 'us-east-1'
