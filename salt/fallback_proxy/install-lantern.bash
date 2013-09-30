c4048233257ddb357ddb3faa19cf797e8088bd0a
Spider Person <aranhoide@gmail.com>
2013-09-30 17:38:10 +0200

    Fix error checking in install-lantern.bash.

--- salt/fallback_proxy/install-lantern.bash
+++ salt/fallback_proxy/install-lantern.bash
@@ -3,6 +3,8 @@
 BUCKET={{ pillar['installer_bucket'] }}
 FILENAME={{ pillar['installer_filename'] }}
 
+set -e
+
 wget -cq https://s3.amazonaws.com/${BUCKET}/${FILENAME}
 dpkg -i $FILENAME
 rm $FILENAME
