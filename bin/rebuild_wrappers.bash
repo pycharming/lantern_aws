795cf5c1dce2121fc9a48f47474d9223b8326509
Spider Person <aranhoide@gmail.com>
2013-11-07 17:47:41 +0100

    rebuild_wrappers.bash: add printout of possible errors.

--- bin/rebuild_wrappers.bash
+++ bin/rebuild_wrappers.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-$(dirname $0)/update.py && $(dirname $0)/ssh_cloudmaster.py 'sudo salt "fp-*" cmd.run "rm /home/lantern/wrappers_built /home/lantern/uploaded_wrappers /home/lantern/reported_completion"' && $(dirname $0)/ssh_cloudmaster.py 'sudo salt "fp-*" state.highstate'
+$(dirname $0)/update.py && $(dirname $0)/ssh_cloudmaster.py 'sudo salt "fp-*" cmd.run "rm /home/lantern/wrappers_built /home/lantern/uploaded_wrappers /home/lantern/reported_completion"' && $(dirname $0)/ssh_cloudmaster.py 'sudo salt "fp-*" state.highstate' | tee hslog ; echo "If there is any printout below, you may want to inspect the hslog file for errors:" ; grep False hslog
