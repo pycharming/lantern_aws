6af00f7416cc06323f833c0ab39519e071eeeb9a
Spider Person <aranhoide@gmail.com>
2014-02-18 19:20:26 +0100

    Record output from wrapper building script.

--- salt/fallback_proxy/build-wrappers.bash
+++ salt/fallback_proxy/build-wrappers.bash
@@ -2,5 +2,5 @@
 
 set -e
 cd /home/lantern/wrapper-repo
-source ../secure/env-vars.txt && ./buildInstallerWrappers.bash
+source ../secure/env-vars.txt && ./buildInstallerWrappers.bash >> /home/lantern/build-wrappers.log 2>&1
 
