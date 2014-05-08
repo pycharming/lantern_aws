b880800cde83cc0c2f6360b5632c53023ae2aaae
Spider Person <aranhoide@gmail.com>
2014-05-08 14:12:17 +0000

    do highstate in batches of 10, not to overload the cloudmaster

--- bin/histate.bash
+++ bin/histate.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-$(dirname $0)/ssh_cloudmaster.py 'sudo salt "fp-*" state.highstate' | tee hslog
+$(dirname $0)/ssh_cloudmaster.py 'sudo salt -b 10 "fp-*" state.highstate' | tee hslog
