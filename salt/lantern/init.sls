e9c66624722f81ea4f2c1bde99e4e20aca0291e6
Spider Person <aranhoide@gmail.com>
2014-05-10 10:00:41 +0200

    fix path to install-lantern-from-git.bash
    This was broken when moving this from fallback_proxy to a common
    .sls that could be shared with the cloudmaster.  It worked for me
    locally because somehow I still had the file in the old location.
    
    Also, remove the renaming of check_lantern.py.  It is no longer the
    case that it will kill all java processes, so it shouldn't interfere
    with the building of Lantern.

--- salt/lantern/init.sls
+++ salt/lantern/init.sls
@@ -1,7 +1,7 @@
 
 install-lantern:
     cmd.script:
-        - source: salt://fallback_proxy/install-lantern-from-git.bash
+        - source: salt://lantern/install-lantern-from-git.bash
         - unless: "[ -e /home/lantern/lantern-repo/target ] && [ \"$(find /home/lantern/lantern-repo/target -maxdepth 1 -name 'lantern-*.jar')\" ]"
         - user: lantern
         - group: lantern
--- salt/lantern/install-lantern-from-git.bash
+++ salt/lantern/install-lantern-from-git.bash
@@ -4,20 +4,12 @@ LOG=$(dirname $0)/install-lantern.log
 
 set -e
 
-# In trying to restart Lantern every minute, this is killing all java processes.
-# Let's disable it so it doesn't kill the building of Lantern.
-[ ! -e check_lantern.py ] || (mv check_lantern.py check_lantern.bak ; sleep 5s)
-
-[ ! -e lantern-repo ] || rm -rf lantern-repo
+[ ! -e lantern-repo ] || rm -rf lantern-repo > $LOG 2>&1
 git clone --depth 1 --recursive --branch {{ pillar.get('branch', 'fallback') }} git://github.com/getlantern/lantern.git lantern-repo > $LOG 2>&1
 cd lantern-repo
 # Symlink pt to the right version of pt in install to get PluggableTransports support
 ln -s install/linux_x86_64/pt pt
 ./install.bash >> $LOG 2>&1
-
-# Restore check_lantern iff we've built Lantern successfully.
-cd $(dirname $0)
-[ ! -e check_lantern.bak ] || mv check_lantern.bak check_lantern.py
 
 echo
 echo 'changed=yes'
