36bccc1a9f744990e781db59402bf3c370577a6c
Ox Cart <ox.to.a.cart@gmail.com>
2014-06-05 16:22:28 -0500

    getlantern/lantern#1576 Not hardcoding path to maven

--- bin/reinstall_lantern.bash
+++ bin/reinstall_lantern.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Update from git, build, restart
-$(dirname $0)/ssh_cloudmaster.py 'sudo salt "fp-*" cmd.run "export PATH=/usr/local/apache-maven-3.1.1/bin/:$PATH && cd /home/lantern/lantern-repo && git pull && git submodule update && ./install.bash && sudo service lantern stop; sudo service lantern start"'
+$(dirname $0)/ssh_cloudmaster.py 'sudo salt "fp-*" cmd.run "export PATH=$PATH:$(find /usr/local -iname 'apache-maven-*')/bin && cd /home/lantern/lantern-repo && git pull && git submodule update && ./install.bash && sudo service lantern stop; sudo service lantern start"'
