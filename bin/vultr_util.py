7911f91e3c5a9e367902607e73f2a0b7b51240d8
Alan Hoyte <aranhoide@gmail.com>
2015-04-09 02:30:31 +0200

    scripts to semi-manually add Vultr servers

---
+++ bin/vultr_util.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import os
+
+from vultr.vultr import Vultr
+
+
+api_key = "XXX"
+tokyo_dcid = u'25'
+planid_768mb = u'31'
+planid_1gb = u'32'
+ubuntu14_10_64bit = u'181'
+aranhoide_ssh_key_id = u'55255a40b2742'
+#aranhoide_ssh_key_id = u'aranhoide'
+label = 'fp-'
+
+bootstrap_tmpl = "ssh -o StrictHostKeyChecking=no root@%s 'curl -L https://bootstrap.saltstack.com | sh -s -- -A 188.166.52.119 -i %s git v2014.7.0'"
+#bootstrap_tmpl = "ssh -o StrictHostKeyChecking=no root@%s 'curl -L https://bootstrap.saltstack.com | sh -s -- -A 128.199.93.248 -i %s git v2014.7.0'"
+
+
+vltr = Vultr(api_key)
+
+def minion_id(prefix, n):
+    return '%s-jp-20150408-%s' % (prefix, str(n).zfill(3))
+
+def create(prefix, start, number):
+    for i in xrange(start, start+number):
+        label = minion_id(prefix, i)
+        print "Creating %s ..." % label
+        print vltr.server_create(tokyo_dcid,
+                                 planid_1gb,
+                                 ubuntu14_10_64bit,
+                                 label=label,
+                                 enable_private_network="yes",
+                                 sshkeyid=aranhoide_ssh_key_id)
+
+def install_salt(prefix, start, number):
+    server_ips = {d['label']: d['main_ip']
+                  for d in vltr.server_list(None).itervalues()}
+    for i in xrange(start, start+number):
+        label = minion_id(prefix, i)
+        if label not in server_ips:
+            print "No server with label %r" % label
+        elif not server_ips[label]:
+            print "No ip for server %s" % label
+        else:
+            ip = server_ips[label]
+            print "Installing salt in %s (%s) ..." % (label, ip)
+            os.system(bootstrap_tmpl % (ip, label))
+
