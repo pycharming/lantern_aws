eff5fd7d1c3412a78944e80503b0d6a7a052ae8a
Alan Hoyte <aranhoide@gmail.com>
2015-08-12 04:12:26 +0200

    uniformize chained server access data format

--- salt/fallback_proxy/save_access_data.py
+++ salt/fallback_proxy/save_access_data.py
@@ -21,17 +21,19 @@ def log_exceptions(f):
 
 @log_exceptions
 def save_access_data():
-    d = json.load(file('{{ fallback_json_file }}'))
-    d['cert'] = subprocess.check_output(
-            ["keytool", "-exportcert",
-             "-alias", "fallback",
-             "-storepass", "Be Your Own Lantern",
-             "-rfc",
-             "-keystore", "/home/lantern/littleproxy_keystore.jks"])
+    d_in = json.load(file('{{ fallback_json_file }}'))
+    d_out = {'addr': '%s:%s' % (d_in['ip'], d_in['port']),
+             'authtoken': d_in['auth_token'],
+             'cert': subprocess.check_output(
+                 ["keytool", "-exportcert",
+                  "-alias", "fallback",
+                  "-storepass", "Be Your Own Lantern",
+                  "-rfc",
+                  "-keystore", "/home/lantern/littleproxy_keystore.jks"])}
     if PT_TYPE is not None:
-        d['pt'] = {{ pillar.get('pt_props')|python }}
-        d['pt']['type'] = PT_TYPE
-    json.dump(d, file('/home/lantern/access_data.json', 'w'))
+        d_out['pt'] = {{ pillar.get('pt_props')|python }}
+        d_out['pt']['type'] = PT_TYPE
+    json.dump(d_out, file('/home/lantern/access_data.json', 'w'), indent=4)
 
 
 if __name__ == '__main__':
