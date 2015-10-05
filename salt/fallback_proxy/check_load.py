77c674375630df4e8f5f4066fdeae478016a6038
Alan Hoyte <aranhoide@gmail.com>
2015-10-05 16:10:04 +0200

    suppress fallback-alarms noise from old built-in servers

--- salt/fallback_proxy/check_load.py
+++ salt/fallback_proxy/check_load.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
 import os
+import sys
 
 import util
 
+if util.instance_id.startswith('fp-nl-201503'):
+    # These are chronically overloaded from traffic by old clients. We can't do
+    # much about them until we stop supporting those versions, so let's
+    # suppress the noise.
+    sys.exit(0)
 
 report_threshold = 1.3
 split_threshold = 0.9
