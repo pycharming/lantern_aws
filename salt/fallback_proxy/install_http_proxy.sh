626d896be5771bf6d466ddd2b5d48096738861e9
Alan Hoyte <aranhoide@gmail.com>
2015-11-10 23:22:25 +0100

    fix printout

--- salt/fallback_proxy/install_http_proxy.sh
+++ salt/fallback_proxy/install_http_proxy.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 
-echo 'Installing golang 1.5.1 ...'
+echo 'Installing http proxy ...'
 curl -LO https://github.com/getlantern/http-proxy/releases/download/v0.0.1/http-proxy
 chmod u+x http-proxy
 echo 'Done'
