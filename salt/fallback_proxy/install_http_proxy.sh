9683b78bead1ac1dbe5125e4d7f6bcc9c7e36352
Alan Hoyte <aranhoide@gmail.com>
2015-11-11 01:43:34 +0100

    replace existing http-proxy binary atomically

--- salt/fallback_proxy/install_http_proxy.sh
+++ salt/fallback_proxy/install_http_proxy.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env sh
 
 echo 'Installing http proxy ...'
-curl -LO https://github.com/getlantern/http-proxy/releases/download/v0.0.1-mobile/http-proxy
-chmod u+x http-proxy
+rm -f http-proxy.tmp
+curl -L https://github.com/getlantern/http-proxy/releases/download/v0.0.1-mobile/http-proxy -o http-proxy.tmp
+chmod u+x http-proxy.tmp
+mv -f http-proxy.tmp http-proxy
 echo 'Done'
