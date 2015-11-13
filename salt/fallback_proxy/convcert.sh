52d04bf89852b74e64b1efb5b1cfda7e48906d94
Alan Hoyte <aranhoide@gmail.com>
2015-11-13 21:28:00 +0100

    add back -nodes; http-proxy can't read the key otherwise

--- salt/fallback_proxy/convcert.sh
+++ salt/fallback_proxy/convcert.sh
@@ -5,6 +5,6 @@ CERT_PASS="Be Your Own Lantern"
 KEY_FILE=/home/lantern/key.pem
 CERT_FILE=/home/lantern/cert.pem
 keytool -v -importkeystore -srckeystore $KEYSTORE -srcalias fallback --srcstorepass "$CERT_PASS" -destkeystore /tmp/keystore.p12 -deststoretype PKCS12 --deststorepass "$CERT_PASS"
-openssl pkcs12 -in /tmp/keystore.p12 -passin pass:"$CERT_PASS" -out $KEY_FILE -passout pass:"$CERT_PASS" -nocerts
+openssl pkcs12 -in /tmp/keystore.p12 -passin pass:"$CERT_PASS" -out $KEY_FILE -passout pass:"$CERT_PASS" -nocerts -nodes
 keytool -exportcert -alias fallback -storepass "$CERT_PASS" -rfc -keystore /home/lantern/littleproxy_keystore.jks -file $CERT_FILE
 rm /tmp/keystore.p12
