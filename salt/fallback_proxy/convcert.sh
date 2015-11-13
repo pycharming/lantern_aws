c0bf0c92fe319f3696e58830998755e39c120589
fffw <fffw@getlantern.org>
2015-11-13 15:12:54 +0800

    fix generating key and cert

--- salt/fallback_proxy/convcert.sh
+++ salt/fallback_proxy/convcert.sh
@@ -5,7 +5,6 @@ CERT_PASS="Be Your Own Lantern"
 KEY_FILE=/home/lantern/key.pem
 CERT_FILE=/home/lantern/cert.pem
 keytool -v -importkeystore -srckeystore $KEYSTORE -srcalias fallback --srcstorepass "$CERT_PASS" -destkeystore /tmp/keystore.p12 -deststoretype PKCS12 --deststorepass "$CERT_PASS"
-openssl pkcs12 -in /tmp/keystore.p12 -passin pass:"$CERT_PASS" -out $KEY_FILE -passout pass:"$CERT_PASS" -nocerts -nodes
-openssl pkcs12 -in /tmp/keystore.p12 -passin pass:"$CERT_PASS" -out /tmp/cert.pem -passout pass:"$CERT_PASS" -clcerts -nokeys
-openssl x509 -in /tmp/cert.pem -out $CERT_FILE
-rm /tmp/keystore.p12 /tmp/cert.pem
+openssl pkcs12 -in /tmp/keystore.p12 -passin pass:"$CERT_PASS" -out $KEY_FILE -passout pass:"$CERT_PASS" -nocerts
+keytool -exportcert -alias fallback -storepass "$CERT_PASS" -rfc -keystore /home/lantern/littleproxy_keystore.jks -file $CERT_FILE
+rm /tmp/keystore.p12
