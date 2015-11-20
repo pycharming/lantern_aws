4539bf2d6c40330258dcf51e32493603ae6fd94d
Alan Hoyte <aranhoide@gmail.com>
2015-11-20 02:50:27 +0100

    add dosgp1 cloudmaster

--- bin/config.py
+++ bin/config.py
@@ -6,10 +6,11 @@ import here
 
 # Values for production deployment.
 salt_version = 'v2015.5.5'
-production_cloudmasters = ['cm-doams3', 'cm-vltok1']
+production_cloudmasters = ['cm-doams3', 'cm-dosgp1', 'cm-vltok1']
 datacenter = os.getenv("DC", 'doams3')
 cloudmaster_name = 'cm-' + datacenter
 cloudmaster_address = {'doams3':  '188.166.35.238',
+                       'dosgp1': '128.199.84.79',
                        'vltok1': '45.32.14.144'}[datacenter]
 
 # To override values locally, put them in config_overrides.py (not version controlled)
--- salt/reboot.sls
+++ salt/reboot.sls
@@ -3,10 +3,10 @@
     - identifier: reboot
     - user: root
 {# Shoot for 4:30 in Beijing and Tehran; let's hope it's OK for other locations. #}
-{% if pillar['datacenter'] == 'vltok1' %}
-    - hour: 20
-    - minute: 30
-{% else %}
+{% if pillar['datacenter'] == 'doams3' %}
     - hour: 1
     - minute: 0
+{% else %}
+    - hour: 20
+    - minute: 30
 {% endif %}
