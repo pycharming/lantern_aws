b8139b49bee57620f53cc1b41c4d89f5f018345d
Alan Hoyte <aranhoide@gmail.com>
2016-01-14 19:14:31 +0100

    update http-proxy version

--- salt/http_proxy/install_http_proxy.py
+++ salt/http_proxy/install_http_proxy.py
@@ -5,7 +5,7 @@ import stat
 import requests
 
 GH_TOKEN = "{{ pillar['github_token'] }}"
-VERSION = 'v0.0.5'
+VERSION = 'v0.0.5-go.1.5.3'
 
 url = 'https://api.github.com/repos/getlantern/http-proxy-lantern/releases/tags/' + VERSION
 headers = {
