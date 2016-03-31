b9ff1d997a67dd4db322c33fc37394b3557de036
Alan Hoyte <aranhoide@gmail.com>
2016-03-31 03:47:05 +0200

    pin netdata to v1.0.0

--- salt/netdata/install-netdata.bash
+++ salt/netdata/install-netdata.bash
@@ -5,7 +5,7 @@ set -e
 DIR=$(mktemp -d)
 
 cd $DIR
-git clone --depth=1 git://github.com/firehol/netdata.git
+git clone --branch=v1.0.0 --depth=1 git://github.com/firehol/netdata.git
 cd netdata
 ./netdata-installer.sh --dont-start-it --dont-wait --zlib-is-really-here --install /opt
 cd /tmp
