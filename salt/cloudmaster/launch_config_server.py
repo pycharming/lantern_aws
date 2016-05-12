95fc6e49bfede52f839d4c59e28ce35e884e45e4
Alan Hoyte <aranhoide@gmail.com>
2016-05-12 17:27:55 +0200

    set the right plan for production config servers

--- salt/cloudmaster/launch_config_server.py
+++ salt/cloudmaster/launch_config_server.py
@@ -8,7 +8,7 @@ import vps_util
 
 def launch_config_server():
     # XXX: this doesn't perform a complete setup yet.
-    name = vps_util.new_vps_name('cs')
+    name = vps_util.new_vps_name('cs', plan=vps_util.my_cm() + '_8GB')
     d = do_util.create_vps(name)
     if not vps_util.highstate_pid(name):
         print("Highstate not running yet; waiting for a bit just in case...")
