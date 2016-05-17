6b7e40b8751e16c7e67d254f4c34d3e1abaaa31b
Ox Cart <ox.to.a.cart@gmail.com>
2016-05-17 13:19:21 -0500

    Keeping two copies of here.py

--- lib/here.py
+++ lib/here.py
@@ -1,6 +1,4 @@
 import os
 
 here = os.path.dirname(__file__)
-salt_states_path = os.path.join(here, '..', 'salt')
-bootstrap_path = os.path.join(here, '..', 'etc', 'bootstrap.bash')
 secrets_path = os.path.join(here, '..', 'secret')
