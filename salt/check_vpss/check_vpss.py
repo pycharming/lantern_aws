9f3b88a21ce2c721094a3997156df3d2e35c7de6
Alan Hoyte <aranhoide@gmail.com>
2016-06-08 05:04:14 +0200

    fix #202

--- salt/check_vpss/check_vpss.py
+++ salt/check_vpss/check_vpss.py
@@ -6,18 +6,10 @@ import vps_util
 
 
 def vpss_from_cm(cm):
-    try:
-        local_version = file(cm + '_vpss_version').read()
-    except IOError:
-        local_version = None
-    remote_version = redis_shell.get(cm + ':vpss:version') or '0'
-    if local_version == remote_version:
-        return set(map(str.strip, file(cm + '_vpss')))
-    else:
-        ret = redis_shell.lrange(cm + ':vpss', 0, -1)
-        file(cm + '_vpss', 'w').write('\n'.join(ret))
-        file(cm + '_vpss_version', 'w').write(remote_version)
-        return set(ret)
+    return (set(redis_shell.lrange(cm + ':vpss', 0, -1))
+            | set([entry.split('*')[0]
+                   for entry in redis_shell.lrange(cm + ':destroyq', 0, -1)
+                   if not entry.startswith('-1')]))
 
 dcs = {'do': ['doams3', 'dosgp1', 'donyc3', 'dosfo1'],
        'li': ['lisgp1', 'litok1']}
